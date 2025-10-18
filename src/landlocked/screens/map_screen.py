5ab9996e9cb03760fef6b20a390a6f297d0f25d7
Matthew McDermott <mcdermmatt2@myvuw.ac.nz>
2025-10-18 21:29:12 +1300

    Removed marker

--- src/landlocked/screens/map_screen.py
+++ src/landlocked/screens/map_screen.py
@@ -5,6 +5,4 @@ class MapScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         mapview = MapView(zoom=12, lat=-41.2865, lon=174.7762)
-        marker = MapMarker(lat=-41.2865, lon=174.7762)
-        mapview.add_marker(marker)
         self.add_widget(mapview)
